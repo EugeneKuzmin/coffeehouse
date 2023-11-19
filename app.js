025cabef33d41e212a76b993f949007185692196
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-11-19 19:04:29 +0300

    feat:add slider. add mobile app download section

--- app.js
+++ app.js
@@ -10,3 +10,33 @@ dataNavToggle.forEach(btn => {
         })
     })
 })
+
+// carousel
+
+const carousel = document.getElementById('carousel')
+const picClass = document.querySelectorAll('.card__img')[0]
+const picWidth = picClass.clientWidth
+
+const arrowLeft = document.querySelector('#arrowLeft')
+const arrowRight = document.querySelector('#arrowRight')
+
+let currScrollPosition = 0
+
+const scrollWidth = carousel.scrollWidth - carousel.clientWidth
+
+const moveLeft = () => {
+    if(!arrowLeft.disabled) {
+        carousel.scrollLeft -= picWidth
+    }
+}
+
+arrowLeft.addEventListener('click',moveLeft)
+
+
+const moveRight = () => {
+    if(!arrowRight.disabled) {
+        carousel.scrollLeft += picWidth
+    }
+}
+
+arrowRight.addEventListener('click',moveRight)
