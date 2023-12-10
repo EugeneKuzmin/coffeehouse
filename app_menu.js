df7b89fe60cacfaced3efc6ce02353f858e63a68
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-12-10 21:01:58 +0300

    feat:develope dialog layout

--- app_menu.js
+++ app_menu.js
@@ -66,7 +66,160 @@ fetch('./products.json')
             modalPopupImage.appendChild(img)
             modalPopup.appendChild(modalPopupImage)
             
-            modalInfo.textContent = element.name
+            const headingInfo = document.createElement('div');
+            headingInfo.classList.add('font-heading-3');
+            headingInfo.textContent = element.name;
+
+            const descriptionInfo = document.createElement('div')
+            descriptionInfo.textContent = element.description
+            descriptionInfo.classList.add("font-body-medium")
+            modalInfo.appendChild(headingInfo)
+            modalInfo.appendChild(descriptionInfo)
+            
+            const tabItemSmall = document.createElement('div')
+            tabItemSmall.classList.add('tab-item')
+
+            const tabItemIconS = document.createElement('div')
+            tabItemIconS.classList.add("tab-item-image")
+            tabItemIconS.textContent = 'S'
+            
+            const tabItemContentS = document.createElement('span')
+            tabItemContentS.classList.add("font-link-n-button")
+            tabItemContentS.textContent = element.sizes.s.size
+
+            tabItemSmall.appendChild(tabItemIconS)
+            tabItemSmall.appendChild(tabItemContentS)
+
+            const tabItemMedium = document.createElement('div')
+            tabItemMedium.classList.add('tab-item')
+
+            const tabItemIconM = document.createElement('div')
+            tabItemIconM.classList.add("tab-item-image")
+            tabItemIconM.textContent = 'M'
+
+            const tabItemContentM = document.createElement('span')
+            tabItemContentM.classList.add("font-link-n-button")
+            tabItemContentM.textContent = element.sizes.m.size
+
+            tabItemMedium.appendChild(tabItemIconM)
+            tabItemMedium.appendChild(tabItemContentM)
+
+
+            const tabItemLarge = document.createElement('div')
+            tabItemLarge.classList.add('tab-item')
+
+            const tabItemIconL = document.createElement('div')
+            tabItemIconL.classList.add("tab-item-image")
+            tabItemIconL.textContent = 'L'
+
+            const tabItemContentL = document.createElement('span')
+            tabItemContentL.classList.add("font-link-n-button")
+            tabItemContentL.textContent = element.sizes.l.size
+
+            tabItemLarge.appendChild(tabItemIconL)
+            tabItemLarge.appendChild(tabItemContentL)
+
+            const sizeButtons = document.createElement('div')
+            sizeButtons.classList.add('flex')
+
+            sizeButtons.appendChild(tabItemSmall)
+            sizeButtons.appendChild(tabItemMedium)
+            sizeButtons.appendChild(tabItemLarge)
+
+
+            //////////////////////////////////////////////
+
+            const tabItemAdd1 = document.createElement('div')
+            tabItemAdd1.classList.add('tab-item')
+
+            const tabItemIcon1 = document.createElement('div')
+            tabItemIcon1.classList.add("tab-item-image")
+            tabItemIcon1.classList.add("font-link-n-button")
+            tabItemIcon1.textContent = '1'
+            
+            const tabItemContent1 = document.createElement('span')
+            tabItemContent1.classList.add("font-link-n-button")
+            tabItemContent1.textContent = element.additives[0].name
+
+            tabItemAdd1.appendChild(tabItemIcon1)
+            tabItemAdd1.appendChild(tabItemContent1)
+
+            const tabItemAdd2 = document.createElement('div')
+            tabItemAdd2.classList.add('tab-item')
+
+            const tabItemIcon2 = document.createElement('div')
+            tabItemIcon2.classList.add("tab-item-image")
+            tabItemIcon2.classList.add("font-link-n-button")
+            tabItemIcon2.textContent = '2'
+
+            const tabItemContent2 = document.createElement('span')
+            tabItemContent2.classList.add("font-link-n-button")
+            tabItemContent2.textContent = element.additives[1].name
+
+            tabItemAdd2.appendChild(tabItemIcon2)
+            tabItemAdd2.appendChild(tabItemContent2)
+
+
+            const tabItemAdd3 = document.createElement('div')
+            tabItemAdd3.classList.add('tab-item')
+
+            const tabItemIcon3 = document.createElement('div')
+            tabItemIcon3.classList.add("tab-item-image")
+            tabItemIcon3.classList.add("font-link-n-button")
+            tabItemIcon3.textContent = '3'
+
+            const tabItemContent3 = document.createElement('span')
+            tabItemContent3.classList.add("font-link-n-button")
+            tabItemContent3.textContent = element.additives[2].name
+
+            tabItemAdd3.appendChild(tabItemIcon3)
+            tabItemAdd3.appendChild(tabItemContent3)
+
+            const addButtons = document.createElement('div')
+            addButtons.classList.add('flex')
+
+            addButtons.appendChild(tabItemAdd1)
+            addButtons.appendChild(tabItemAdd2)
+            addButtons.appendChild(tabItemAdd3)
+
+            ///////////////////////////////////////////
+
+            const totalBlock = document.createElement('div')
+            totalBlock.classList.add('font-heading-3')
+            totalBlock.classList.add('flex')
+            totalBlock.classList.add('justify-content-between')
+            const totalText = document.createElement('div')
+            totalText.textContent = 'Total'
+            const totalPrice = document.createElement('div')
+            totalPrice.textContent = '7.00$'
+            totalBlock.appendChild(totalText)
+            totalBlock.appendChild(totalPrice)
+            
+            /////////////////////////////////////
+            
+            const priceInfo = document.createElement('div')
+            priceInfo.classList.add('font-body-caption')
+            priceInfo.textContent = 'The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.'
+            
+            /////////////////////////////////////
+            
+            const buttonCloseBlock = document.createElement('div')
+            const buttonClose = document.createElement('button')
+
+            buttonCloseBlock.appendChild(buttonClose)
+
+            buttonClose.classList.add('button-secondary')
+            buttonClose.classList.add('font-link-n-button')
+            buttonClose.textContent = 'Close'
+
+            modalInfo.appendChild(sizeButtons)
+            modalInfo.appendChild(addButtons)
+            modalInfo.appendChild(totalBlock)
+            modalInfo.appendChild(priceInfo)
+            modalInfo.appendChild(buttonCloseBlock)
+
+            
+
             modalPopup.appendChild(modalInfo)
         }
 
